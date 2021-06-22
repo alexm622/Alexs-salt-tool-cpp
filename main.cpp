e70c4e9cfa14e76ab1391f899a76e5606a0a4e69
Alex-Remi <alex@remi-it.com>
2021-06-21 20:09:12 -0400

    basic starting work
    added the motd, a read and started to work on options

--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
-#include <iostream>
+//    _   _           _      __       _ _     _____            _
+//   /_\ | | _____  _( )__  / _\ __ _| | |_  /__   \___   ___ | |
+//  //_\\| |/ _ \ \/ // __| \ \ / _` | | __|   / /\/ _ \ / _ \| |
+// /  _  \ |  __/>  < \__ \ _\ \ (_| | | |_   / / | (_) | (_) | |
+// \_/ \_/_|\___/_/\_\|___/ \__/\__,_|_|\__|  \/   \___/ \___/|_|
 
-using namespace std;
+
+#include <iostream>
+#include <vector>
+
+struct Option {
+    std::string text;
+    int id;
+};
+
+std::vector<Option> Options;
+
+std::string read(){
+    std::string s;
+    std::cin >> s;
+
+    return s;
+}
+
+void motd(){
+    printf("   _   _           _      __       _ _     _____            _ \n");
+    printf("  /_\\ | | _____  _( )__  / _\\ __ _| | |_  /__   \\___   ___ | |\n");
+    printf(" //_\\\\| |/ _ \\ \\/ // __| \\ \\ / _\\ | | __|   / /\\/ _ \\ / _ \\| |\n");
+    printf("/  _  \\ |  __/>  < \\__ \\ _\\ \\ (_| | | |_   / / | (_) | (_) | |\n");
+    printf("\\_/ \\_/_|\\___/_/\\_\\|___/ \\__/\\__,_|_|\\__|  \\/   \\___/ \\___/|_|\n");
+    printf("                                                              \n");
+}
+
+void initOptions(){
+    int i = 0;
+
+}
+
+void printOptions(){
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    motd();
+
+    std::string s = read();
+    printf(s.c_str());
+
 }
+
